f044628c5d93e754e86b556bdeab30d6f136e1b8
Roch Armengaud <armengaud.roch@live.fr>
2022-06-04 16:44:21 +0200

    Debut de code sur index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,52 +1,82 @@
 class Api {
   constructor(url) {
-    this._url = url;
+    this.url = url;
   }
 
   async get() {
-    return fetch(this._url)
-      .then((res) => res.json())
-      .then((res) => res.photographers)
-      .catch((err) => console.log('an error occurs', err));
+    const httpResponse = await fetch(this.url);
+    const httpData = await httpResponse.json();
+    return httpData;
   }
 }
 // on va chercher les donnees dans le json
 
-class MovieCard {
-  constructor(movie) {
-    this._movie = movie;
+class PhotographerCard {
+  constructor(card) {
+    this.card = card;
     // des qu'on crée un constructor, on utilisera this
   }
 
-  createMovieCard() {
-    const $wrapper = document.querySelector('.photographer-wrapper');
+  createPhotographerCard() {
+    const wrapper = document.querySelector('.photographer-wrapper');
 
-    const movieCard =
-      `<h3>` +
-      this._movie.name +
-      `</h3><img src="/assets/photographers/Photographers ID Photos/` +
-      this._movie.portrait +
-      `" />`;
+    const photographerCardWrapper = document.createElement('div');
+    photographerCardWrapper.className = 'photographer-card-wrapper';
 
-    $wrapper.innerHTML += movieCard;
+    /**
+     * CARD PORTRAIT
+     */
+    const photographerPicture = document.createElement('img');
+    photographerPicture.src =
+      '/assets/photographers/Photographers ID Photos/' + this.card.portrait;
+
+    const photographerName = document.createElement('h1');
+    photographerName.appendChild(document.createTextNode(this.card.name));
+
+    const photographerPortrait = document.createElement('div');
+    photographerPortrait.className = 'photographer-portrait';
+    photographerPortrait.appendChild(photographerPicture);
+    photographerPortrait.appendChild(photographerName);
+    /**
+     * CARD INFO
+     */
+
+    // les append font apparaitre la div et sa classe
+    photographerCardWrapper.appendChild(photographerPortrait);
+    wrapper.appendChild(photographerCardWrapper);
+
+    // `<h1>` +
+    // this.card.name +
+    // `</h1><img src="/assets/photographers/Photographers ID Photos/` +
+    // this.card.portrait +
+    // `" />` +
+    // '<h3>' +
+    // this.card.city;
+
+    // wrapper.innerHTML += photographerCard;
   }
 }
 
 class App {
   constructor() {
-    this.moviesApi = new Api('/data/photographers.json');
-    // les classes demarrent toujours avec une majuscule (ici MovieApi)
+    this.photographerCardApi = new Api('/data/photographers.json');
   }
 
   async main() {
-    const movies = await this.moviesApi.get();
+    const { photographers: cards } = await this.photographerCardApi.get();
+    for (const card of cards) {
+      const template = new PhotographerCard(card);
+      template.createPhotographerCard();
+    }
 
-    movies.forEach((movie) => {
-      const Template = new MovieCard(movie);
-      Template.createMovieCard();
-    });
+    // cards.forEach((card) => {
+    //   const template = new PhotographerCard(card);
+    //   template.createPhotographerCard();
+    // });
   }
 }
 
 const app = new App();
 app.main();
+
+// new App().main()
