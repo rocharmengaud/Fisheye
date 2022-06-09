fcb50657efeb7b0b141747104c45fe37b87fe783
Roch Armengaud <armengaud.roch@live.fr>
2022-06-09 15:49:38 +0200

    photographer.html profiles setup complete

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -17,11 +17,45 @@ class PhotographerProfile {
   createPhotographerProfile() {
     const header = document.querySelector('.photograph-header');
 
+    const profile = document.createElement('div');
+    profile.className = 'profile';
+
     const profileName = document.createElement('div');
     profileName.className = 'profile-name';
     profileName.appendChild(document.createTextNode(this.profile.name));
 
+    const profileInfo = document.createElement('div');
+    profileInfo.className = 'profile-info';
+
+    const profileLocation = document.createElement('div');
+    profileLocation.className = 'profile-location';
+
+    const profileCity = document.createElement('div');
+    profileCity.className = 'profile-city';
+    profileCity.appendChild(document.createTextNode(this.profile.city));
+    const profileCountry = document.createElement('div');
+    profileCountry.className = 'profile-country';
+    profileCountry.appendChild(document.createTextNode(', ' + this.profile.country));
+
+    const profileTagline = document.createElement('div');
+    profileTagline.className = 'profile-tagline';
+    profileTagline.appendChild(document.createTextNode(this.profile.tagline));
+
+    const profilePicture = document.createElement('img');
+    profilePicture.className = 'profile-picture';
+    profilePicture.src =
+      '/assets/photographers/Photographers ID Photos/' + this.profile.portrait;
+
+    header.appendChild(profile);
     header.appendChild(profileName);
+    header.appendChild(profileInfo);
+    header.appendChild(profilePicture);
+    profile.appendChild(profileName);
+    profile.appendChild(profileInfo);
+    profileInfo.appendChild(profileLocation);
+    profileInfo.appendChild(profileTagline);
+    profileLocation.appendChild(profileCity);
+    profileLocation.appendChild(profileCountry);
   }
 }
 
@@ -47,7 +81,3 @@ class App {
 
 const app = new App();
 app.main();
-
-// recup id url
-// recup json
-// créer la page en affichant les données du json correspondant a l'id
