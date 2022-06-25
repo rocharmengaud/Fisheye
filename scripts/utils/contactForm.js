60af193b9308575419a1cab8e81c7b8d6e0831ff
Roch Armengaud <armengaud.roch@live.fr>
2022-06-25 17:16:49 +0200

    wip modal submission

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -11,3 +11,14 @@ function closeModal() {
   modal.style.display = 'none';
   overlay.style.display = 'none';
 }
+
+const submit = document.querySelector('.submit_button');
+const first = document.querySelector('#first');
+const last = document.querySelector('#last');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+submit.addEventListener('click', (event) => {
+  closeModal();
+  // console.log(first.value, last.value, email.value, message.value);
+});
