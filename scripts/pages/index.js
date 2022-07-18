30bab9aed0267fb2d2ebdb1fd56e336048891eb7
Roch Armengaud <armengaud.roch@live.fr>
2022-07-18 16:32:29 +0200

    focussable elements on index

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -22,7 +22,7 @@ class PhotographerCard {
     const wrapper = document.querySelector('.photographer-wrapper');
 
     const photographerCardWrapper = document.createElement('div');
-    photographerCardWrapper.className = 'photographer-card-wrapper center';
+    photographerCardWrapper.className = 'photographer-card-wrapper center focussable';
     /**
      * CARD PORTRAIT
      */
@@ -101,6 +101,28 @@ class App {
     // });
   }
 }
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Tab') {
+    //add all elements we want to include in our selection
+    var focussableElements =
+      'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
+    if (document.activeElement && document.activeElement.form) {
+      var focussable = Array.prototype.filter.call(
+        document.activeElement.form.querySelectorAll(focussableElements),
+        function (element) {
+          //check for visibility while always include the current activeElement
+          return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement;
+        }
+      );
+      var index = focussable.indexOf(document.activeElement);
+      if (index > -1) {
+        var nextElement = focussable[index + 1] || focussable[0];
+        nextElement.focus();
+      }
+    }
+  }
+});
 
 const app = new App();
 app.main();
